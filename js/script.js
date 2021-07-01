20db4ff0a6145946f1eb1dc05d6a49b2c2b35b69
DianaJS12 <dianakanybekkyzy32@gmail.com>
2021-07-01 20:49:29 +0600

    first add

---
+++ js/script.js
@@ -0,0 +1,78 @@
+// var a = +prompt('введите количество участников')
+// var arr = []
+// let arr2 = ''
+// let str 
+// function num (){
+//     for(var i = 0; i < a; i++){
+//         let b = prompt('enter name')
+//         arr[i] = b
+        
+//     }
+//     let ask = prompt('name')
+    
+//     for(var  i = 0; i < arr.length; i++){
+//         if(ask === arr[i]){
+//             arr2 += arr[i]
+//         }
+//         str = ask.split("").reverse().join("")
+//     }
+//     console.log(arr);
+//     console.log(arr2);
+//     console.log(str);
+// }
+
+// const num = () => {
+//     for(var i = 0; i < a; i++){
+//         let b = prompt('enter name')
+//         arr[i] = b
+        
+//     }
+//     let ask = prompt('name')
+    
+//     for(var  i = 0; i < arr.length; i++){
+//         if(ask === arr[i]){
+//             arr2 += arr[i]
+//         }
+//         str = ask.split("").reverse().join("")
+//     }
+//     console.log(arr);
+//     console.log(arr2);
+//     console.log(str);
+// }
+
+
+// num()
+
+
+// var arr = ['muni' , 23 , true , false  , 36 , 'dianas' , 23 , 'bael' , 'beksultan']
+// let newarr = []
+// const num = () => {
+//     for(let i =0; i < arr.length; i++){
+//         if(typeof arr[i] === 'string'){
+//             if(arr[i].length > 5){
+//                 newarr.push(arr[i])
+//             }else if(arr[i].length <= 5){
+//                 arr[i] = 0
+//             }
+//         }
+//     }
+// }
+// num()
+// console.log(arr);
+// console.log(newarr);
+// console.log(newarr.join(" ,"));
+
+// let a = +prompt('enter')
+
+// function res(item){
+//     return -Math.abs(a)
+// }
+// res()
+// console.log(res(a));
+
+// let a = +prompt('num')
+// const res = item => {
+//     return -Math.abs(item)
+// }
+
+// console.log(res(a));
